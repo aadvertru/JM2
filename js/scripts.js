ec18e6dcade6573f224fd544054ad34eea96aec3
aadvertru <aadvertru@gmail.com>
2020-10-15 22:42:22 +0300

    slider added

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,46 @@
-console.log('as')
-let readMore = document.getElementById('readMore')
-readMore.onclick = () => {
-    document.querySelector('.content-text').classList.toggle("showAll")
-    readMore.classList.toggle('shown')
-    if (readMore.textContent === 'Читать далее') {
-        readMore.textContent = 'Свернуть'
+const readMore = document.getElementById('readMore')
+const showMore = document.getElementById('showMore')
+
+function showAll(selector, textHidden, textShowed) {
+    document.getElementById('contentAll').classList.toggle("showAll")
+    selector.classList.toggle('shown')
+    if (selector.textContent === textHidden) {
+        selector.textContent = textShowed
     } else {
-        readMore.textContent = 'Читать далее'
+        selector.textContent = textHidden
     }
     console.log('clicked')
 }
+
+readMore ? readMore.onclick = () => showAll(readMore, 'Читать далее', 'Свернуть') : null
+showMore ? showMore.onclick = () => showAll(showMore, 'Показать все', 'Скрыть') : null
+
+
+// SWIPER ON RESIZE
+const breakpoint = window.matchMedia( '(min-width:321px)' );
+let mySwiper;
+
+const breakpointChecker = function() {
+    if ( breakpoint.matches === true ) {
+        if ( mySwiper !== undefined ) mySwiper.destroy( true, true );
+        return;
+    } else if ( breakpoint.matches === false ) {
+        return enableSwiper();
+    }
+};
+
+const enableSwiper = function() {
+    mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        slidesPerView: 'auto',
+        keyboardControl: true,
+        grabCursor: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        paginationClickable: true,
+    });
+};
+
+breakpoint.addListener(breakpointChecker);
+breakpointChecker();
